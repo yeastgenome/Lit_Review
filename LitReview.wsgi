4dd3f39c7c1d0ff0b7de82b050a998bee0e6b25d
kpaskov <kpaskov@stanford.edu>
2013-01-16 09:19:39 -0800

    wsgi and setup changes.

--- LitReview.wsgi
+++ LitReview.wsgi
@@ -1,1 +1,6 @@
+import sys
+sys.stdout=sys.stderr
+
 from webapp.main import app as application
+application.debug=True
+application.secret_key = 'my secret key'
--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ tests_require = [
 
 setup(
     name='LitReview',
-    version='0.1dev',
+    version='0.7dev',
     description='Literature curation tool.',
     long_description=README,
     author='Kelley Paskov',
